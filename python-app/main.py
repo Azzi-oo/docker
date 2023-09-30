fc0677d3c3144640eb118310eec2de52fb628959
Azat Yanberdin <azatdjon@yandex.ru>
2023-09-30 23:59:38 +0500

    add new docker file

---
+++ python-app/main.py
@@ -0,0 +1,9 @@
+import calendar
+print('Welcome in calendar\n')
+
+year = int(input('Please input year: '))
+month = int(input('Input number month: '))
+
+print(calendar.month(year, month))
+
+print('All goods!')
